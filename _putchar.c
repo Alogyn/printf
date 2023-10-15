d66fc6a5dee91283868368c6cef17db9eb398612
AbdouFl <abderrahimfilaliofficial@gmail.com>
2023-10-15 13:50:44 -0700

    Add the characters to a buffer and prints them

--- _putchar.c
+++ _putchar.c
@@ -1,5 +1,18 @@
 #include "main.h"
 
+
+/**
+* _putchar - Add the characters to a buffer and
+* prints them
+*
+* @buffer: Buffer array (char)
+* @c: Input character (char)
+* @index: Number of buffer index
+*
+* Return: The number of buffer index
+*/
+
+
 unsigned int _putchar(char *buffer, char c, unsigned int index)
 {
 	if (index >= 1024)
